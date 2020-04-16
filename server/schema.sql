bb559b9e0309034e3bde1e1d615171dfeab37983
Steve Marquez <steveantonio00@gmail.com>
2020-04-15 17:31:16 -0700

    Create schema and add client folder

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,25 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+ messageID int NOT NULL AUTO_INCREMENT,
+ userID int NOT NULl,
+ roomname VARCHAR(20) NOT NULL,
+ bodyMessage VARCHAR(300) NOT NULL,
+ PRIMARY KEY (messageID)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE user (
+  userID int NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(20) NOT NULL,
+  PRIMARY KEY (userID)
+);
 
 
 /*  Execute this file from the command line by typing:
